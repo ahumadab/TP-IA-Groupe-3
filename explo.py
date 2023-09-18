cc66d062b4357523b6edfd25b070ccd40046a700
Boris <boris.ahumada@laposte.net>
2023-09-18 15:57:24 +0200

    adding method to train and save models

--- explo.py
+++ explo.py
@@ -5,24 +5,47 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 from helpers import correlation, correlation_unique, courbe_en_fonction, courbe_en_fonction_LinearRegression, \
-    courbe_en_fonction_PolynomialFeatures
+    courbe_en_fonction_PolynomialFeatures, patrick
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
 df = pd.read_csv("./beers/recipeData.csv", encoding="latin1")
 # orig_leng = len(df)
+# print(orig_leng)
 df = df.drop(
     columns=["BeerID", "UserId", "URL", "Name", "Style", "PrimingMethod", "PrimingAmount", "PitchRate", "MashThickness",
              "PrimaryTemp", "SugarScale"])  # PrimaryTemp
+# ser = df.isna().mean() * 100
+# print(ser)
 df.dropna(inplace=True)
-ser = df.isna().mean() * 100
+# ser = df.isna().mean() * 100
+# print(ser)
+#
+# orig_leng = len(df)
+# print(ser)
+# og_df = df[["OG"]]
+# print(og_df.describe(include='all'))
+# og_df = og_df.sort_values(by="OG", ascending=True)
+# plt.plot(range(len(og_df)), og_df, marker=",")
+# plt.xlabel("Pourcentage")
+# pourcentages = [str(i) + '%' for i in range(0, 101, 10)]
+# plt.xticks(range(0, len(df), len(df) // 10), pourcentages)
+# plt.ylabel('OG (Densité du moût avant fermentation)')
+# plt.title("Courbe triée des valeurs de OG")
+# plt.grid()
+# plt.show()
 
 df = df[df["Size(L)"] <= df["Size(L)"].quantile(0.95)]
 df = df[df["OG"] <= df["OG"].quantile(0.95)]
 df = df[df["FG"] <= df["FG"].quantile(0.95)]
 df = df[df["IBU"] <= 150]  # IBU max == 150 selon wikipedia
 df = df[df["BoilSize"] <= df["BoilSize"].quantile(0.95)]
+# df = df[df["IBU"] > 5]
+
+# new_len = len(df)
+# print(new_len / orig_leng, new_len)
+
 
 df.rename(columns={"OG": "Density of Wort b4 ferm", "FG": "Density of Wort after ferm", "ABV": "Alcohol By Volume", "IBU": "International Bittering Units"}, inplace=True)
 
@@ -33,7 +56,6 @@ df = df.join(one_hot)
 # print(df)
 
 
-# print(df.describe(include='all'))
 
 # correlation(df)
 #
@@ -43,24 +65,37 @@ df = df.join(one_hot)
 
 
 # plt.subplot()
-df_alcool = df["Alcohol By Volume"]
-df_amertume = df["International Bittering Units"]
+ser_alcool = df["Alcohol By Volume"]
+ser_amertume = df["International Bittering Units"]
 
 df_desity_before_ferm = df[["Density of Wort b4 ferm"]]
 df_desity_after_ferm = df[["Density of Wort after ferm"]]
 df_boil_time = df[["BoilTime"]]
 
 df_density = df[["Density of Wort b4 ferm", "Density of Wort after ferm"]]
+df_alcool = df[["Alcohol By Volume"]]
+df_amertume = df[["International Bittering Units"]]
 
-courbe_en_fonction_LinearRegression(df_desity_before_ferm, df_alcool)
-courbe_en_fonction_PolynomialFeatures(df_desity_after_ferm, df_alcool, degree=2)
-courbe_en_fonction_PolynomialFeatures(df_boil_time, df_alcool, degree=3)
-print(df_density)
+# courbe_en_fonction_LinearRegression(df_desity_before_ferm, df_alcool)
+# courbe_en_fonction_PolynomialFeatures(df_desity_after_ferm, df_alcool, degree=2)
+# courbe_en_fonction_PolynomialFeatures(df_boil_time, df_alcool, degree=3)
+
+
+# courbe_en_fonction_PolynomialFeatures(df_amertume, ser_alcool, degree=5)
+
+# print(df_density)
 # courbe_en_fonction_PolynomialFeatures(df_density, df_alcool, degree=1)
 
-courbe_en_fonction_PolynomialFeatures(df_desity_before_ferm, df_amertume, degree=10)
-courbe_en_fonction_PolynomialFeatures(df_desity_after_ferm, df_amertume, degree=7)
-courbe_en_fonction_PolynomialFeatures(df_boil_time, df_amertume, degree=4)
+# courbe_en_fonction_PolynomialFeatures(df_desity_before_ferm, df_amertume, degree=10)
+# courbe_en_fonction_PolynomialFeatures(df_desity_after_ferm, df_amertume, degree=7)
+# courbe_en_fonction_PolynomialFeatures(df_boil_time, df_amertume, degree=4)
+# courbe_en_fonction_PolynomialFeatures(df_alcool, ser_amertume, degree=2)
 # courbe_en_fonction(df_density, df_amertume)
 
 # plt.show()
+X = df[["Size(L)", "Density of Wort b4 ferm", "Density of Wort after ferm", "BoilSize", "BoilTime", "BoilGravity", "Efficiency"]]
+y_ibu = df["International Bittering Units"]
+y_abv = df["Alcohol By Volume"]
+
+
+patrick(X, y_ibu, y_abv)
--- helpers.py
+++ helpers.py
@@ -1,16 +1,28 @@
 from typing import List
 
 import sklearn.preprocessing
+import skl2onnx
+from skl2onnx import convert, to_onnx
+from skl2onnx.common.data_types import FloatTensorType
 from pandas import DataFrame, Series
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV, train_test_split
+import onnxruntime as ort
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestRegressor
 import seaborn as sns
 import numpy as np
+import sys
 
+
+np.set_printoptions(threshold=sys.maxsize)
 
 def correlation(df: DataFrame):
     corr_df = df.corr()
+    print(corr_df)
     plt.matshow(corr_df)
     plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14,
                rotation=90)
@@ -61,7 +73,6 @@ def courbe_en_fonction_PolynomialFeatures(df_x: DataFrame, df_y: Series, degree=
 
     poly_features = PolynomialFeatures(degree=degree)
     x_poly = poly_features.fit_transform(df_x)
-    print(x_poly, df_y)
     model = LinearRegression()
     model.fit(x_poly, df_y)
     y_predict = model.predict(x_poly)
@@ -80,3 +91,86 @@ def courbe_en_fonction_PolynomialFeatures(df_x: DataFrame, df_y: Series, degree=
     plt.grid(True)
     plt.legend()
     plt.show()
+
+
+def save_model_onnx(model, X):
+    initial_type = [('float_input', FloatTensorType([None, X.shape[1]]))]
+    onnx_model = to_onnx(model, initial_types=initial_type)
+
+    with open("IBU.onnx", "wb") as f:
+        f.write(onnx_model.SerializeToString())
+
+def patrick(X: DataFrame, y_ibu, y_abv):
+    print(X)
+    # model_abv = LinearRegression()
+    # model_abv.fit(X, y_abv)
+    # y_abv_pred = model_abv.predict(X)
+
+    # save_model_onnx(model_abv, X)
+
+    X_test_numpy = X.values.astype(np.float32)
+    print(X_test_numpy[0, :])
+    print(X_test_numpy.shape)
+    model = ort.InferenceSession("ABV.onnx")
+    input_data = {"float_input": X_test_numpy}
+    predictions = model.run(None, input_data)
+
+    X.insert(len(X.columns), "ABV", predictions[0])
+
+    X_train, X_test, y_ibu_train, y_ibu_test = train_test_split(X, y_ibu, test_size=0.2, random_state=42)
+
+    # param_grid = {
+    #     'n_estimators': [100, 200, 300],  # Nombre d'arbres dans la forêt aléatoire
+    #     'max_depth': [None, 10, 20, 30],  # Profondeur maximale de chaque arbre
+    #     'min_samples_split': [2, 5, 10],  # Nombre minimum d'échantillons requis pour diviser un nœud
+    #     'min_samples_leaf': [1, 2, 4]  # Nombre minimum d'échantillons requis dans une feuille
+    # }
+
+    # model_ibu = RandomForestRegressor(n_estimators=100, random_state=42)
+    # grid_search = HalvingGridSearchCV(model_ibu, param_grid, verbose=2, n_jobs=-1, random_state=42)
+    # grid_search.fit(X_train, y_ibu_train)
+    # best_params = grid_search.best_params_ # {'max_depth': 10, 'min_samples_leaf': 4, 'min_samples_split': 2, 'n_estimators': 300}
+    """best_params = {'max_depth': 10, 'min_samples_leaf': 4, 'min_samples_split': 2, 'n_estimators': 300}
+    print("Meilleurs hyperparamètres : ", best_params)
+    best_rf_model = RandomForestRegressor(random_state=42, **best_params)
+    best_rf_model.fit(X_train, y_ibu_train)
+    y_ibu_pred = best_rf_model.predict(X_test)"""
+    X_test_numpy = X_test.values.astype(np.float32)
+    # print(X_test_numpy[0, :])
+    # print(X_test_numpy.shape)
+    model = ort.InferenceSession("IBU.onnx")
+    input_data = {"float_input": X_test_numpy}
+    predictions = model.run(None, input_data)
+    print(predictions)
+    y_ibu_pred = predictions[0]
+    # model_ibu.fit(X_train, y_ibu_train)
+    # y_ibu_pred = model_ibu.predict(X_test)
+    mse_ibu = mean_squared_error(y_ibu_test, y_ibu_pred)
+    r2_ibu = r2_score(y_ibu_test, y_ibu_pred)
+
+    print("mse_ibu\n", mse_ibu)
+    print("r2_ibu\n", r2_ibu)
+    # print(sqrt(mse_ibu))
+
+    # save_model_onnx(best_rf_model, X)
+
+    # x = np.linspace(0, 1, len(y_ibu_test))
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # ax.scatter(x, y_ibu_test, color='yellow', label='Réel', alpha=0.5)
+    #
+    # # Tracer les valeurs prédites en rouge
+    # ax.scatter(x, y_ibu_pred, color='red', label='Prédit', alpha=0.5)
+    #
+    # # Ajouter une légende
+    # ax.legend()
+    #
+    # # Définir les étiquettes des axes
+    # ax.set_xlabel('Échantillons')
+    # ax.set_ylabel('IBU')
+    #
+    # # Titre du graphique
+    # plt.title('Réel vs Prédit (IBU)')
+    #
+    # # Afficher le graphique
+    # plt.show()
+
